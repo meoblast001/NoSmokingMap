fff323ea137c01a79b009df6b376c24624e0cf48
Braden Walters <vc@braden-walters.info>
2025-06-22 19:30:20 +0200

    Basic information page.

--- Client/Pages/AboutPage.tsx
+++ Client/Pages/AboutPage.tsx
@@ -1,11 +1,14 @@
-import { Container } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import * as React from 'react';
 
 export class AboutPage extends React.Component {
   render(): React.ReactNode {
     return (
-      <Container sx={{ p: 2 }}>
-        Lorem ipsum blah blah foo.
+      <Container sx={{ p: 2, textAlign: 'center' }}>
+        <Typography>
+          Copyright &copy; 2025 Braden Walters<br />
+          Licensed <a href="https://en.wikipedia.org/wiki/MIT_License">MIT</a>
+        </Typography>
       </Container>
     )
   }
